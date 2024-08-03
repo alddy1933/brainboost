b25d4d9c2d4a601d733a9bbd727fe6a6b8f7d2e7
alddy1933 <alddy1933@gmail.com>
2024-08-04 02:44:17 +0700

    fix(Dashboard & Quizzes): Change image stamp banner event

--- app/Modules/Quizzes/Views/index.php
+++ app/Modules/Quizzes/Views/index.php
@@ -37,12 +37,11 @@
                         <div class="card-stamp card-stamp-lg">
                             <div class="card-stamp-icon bg-primary">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/ghost -->
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-speakerphone">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M5 11a7 7 0 0 1 14 0v7a1.78 1.78 0 0 1 -3.1 1.4a1.65 1.65 0 0 0 -2.6 0a1.65 1.65 0 0 1 -2.6 0a1.65 1.65 0 0 0 -2.6 0a1.78 1.78 0 0 1 -3.1 -1.4v-7" />
-                                    <line x1="10" y1="10" x2="10.01" y2="10" />
-                                    <line x1="14" y1="10" x2="14.01" y2="10" />
-                                    <path d="M10 14a3.5 3.5 0 0 0 4 0" />
+                                    <path d="M18 8a3 3 0 0 1 0 6" />
+                                    <path d="M10 8v11a1 1 0 0 1 -1 1h-1a1 1 0 0 1 -1 -1v-5" />
+                                    <path d="M12 8h0l4.524 -3.77a.9 .9 0 0 1 1.476 .692v12.156a.9 .9 0 0 1 -1.476 .692l-4.524 -3.77h-8a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h8" />
                                 </svg>
                             </div>
                         </div>
